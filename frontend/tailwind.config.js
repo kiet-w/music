5895443567f7d793271bfdbcb7c17c4e23cddbbc
kiet <kiet.tran@automationland.com.vn>
2026-06-01 20:36:04 +0700

    style: add shimmer animation to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -55,7 +55,15 @@ module.exports = {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
-      }
+      },
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 2s infinite',
+      },
     }
   },
   plugins: [require("tailwindcss-animate")],
