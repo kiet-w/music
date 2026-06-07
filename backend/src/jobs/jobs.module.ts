ee86d0b06247d567f8ec030d277d280a371be1f7
kiet <kiet.tran@automationland.com.vn>
2026-06-07 14:38:15 +0700

    q

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -11,6 +11,7 @@ import { StorageModule } from '../storage/storage.module';
         host: process.env.REDIS_HOST || 'localhost',
         port: parseInt(process.env.REDIS_PORT || '6379'),
         password: process.env.REDIS_PASSWORD,
+        tls: process.env.REDIS_HOST?.includes('upstash') ? {} : undefined,
       },
     }),
     BullModule.registerQueue({
